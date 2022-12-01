a47acad52294cf132a20c609912efdc7e4e2ecd1
NosferaLuk <luk230899@gmail.com>
2022-12-01 12:03:51 -0300

    commit

---
+++ Blog/Program.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Data;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           using(var context = new BlogDataContext())
+           {
+            
+           }
+        }
+    }
+}
